d467d1f99fb6aad610394672c210b127fe946e40
KWCAgoraProgrammeursClub <127244979+KWCAgoraProgrammeursClub@users.noreply.github.com>
2023-03-08 08:10:56 +0100

    Update Voorbeeld_001.c

--- C/Voorbeeld_001.c
+++ C/Voorbeeld_001.c
@@ -14,11 +14,13 @@ int main
  int main  -- het programma retourneert een getal aan degene die het programma opstart. Meestal is dit een shell.
               Het getal dat hier geretourneerd wordt, kun je in de shell opvragen bijv door 'echo $?'.
               
- (int argc, char* argv[])  -- dit is de array can command line arguments die bijvoorbeeld door een shell
+ (int argc, char* argv[])  -- dit is de array van command line arguments die bijvoorbeeld door een shell
                               gegeven kunnen worden. argv[ 0 ] bevat de naam van de executable zelf,
                               argv[ 1 ] etc bevatten de rest van de arguments.
                               
  return 0;  -- hiermee geeft het programma dus de exit code terug aan het systeem. Als je hier '1' van maakt,
                dan zal 'echo $?' in de shell dus 1 geven.
+               
+ Je kunt dit programma compileren door 'cc myfile.c -o myprogram' uit te voeren (en dan ./myprogram om het uit te voeren).
  
 */
